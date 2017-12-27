5d7bd6abae9379d782968e5ded88bacac223faec
Etihwo <etihwo@outlook.com>
2017-12-27 13:50:29 +0100

    Add goosing similar to reporting

---
+++ dotnet/IEC61850forCSharp/Goosing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace IEC61850
+{
+    namespace Client
+    {
+        public partial class IedConnection
+        {
+
+            public GooseControlBlock GetGooseControlBlock(string gocbObjectReference)
+            {
+                var newGoCB = new GooseControlBlock(gocbObjectReference, this, connection);
+
+
+                return newGoCB;
+            }
+
+        }
+    }
+}
