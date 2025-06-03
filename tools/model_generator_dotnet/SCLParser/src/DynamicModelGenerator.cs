c002f9e2ca610f050c7df4e23d2b4ef0a7048578
unknown <romina.roshani@mz-automation.de>
2025-06-03 15:24:12 +0200

    removed comment

--- tools/model_generator_dotnet/SCLParser/src/DynamicModelGenerator.cs
+++ tools/model_generator_dotnet/SCLParser/src/DynamicModelGenerator.cs
@@ -546,11 +546,6 @@ namespace IEC61850.SCL
             if (dataAttribute.AttributeType != AttributeType.CONSTRUCTED)
             {
 
-                if (dataAttribute.ObjRef.Contains("stVal"))
-                {
-                    Console.WriteLine("debug");
-                }
-
                 DataObject dataObject = findDOParent(dataAttribute);
                 LogicalNode logicalNode = findLNParent(dataObject);
                 LogicalDevice logicalDevice = logicalNode.Parent as LogicalDevice;
